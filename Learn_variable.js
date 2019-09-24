940c91108a517a98b21bb1ade216401a7fc0c000
dudq <dudq.xmt@gmail.com>
2019-09-24 23:39:15 +0700

    add Learn_variable.js file

---
+++ Learn_variable.js
@@ -0,0 +1,21 @@
+// Bai 1
+let  i = 10;
+let  f = 20.5;
+let b = true;
+let s = 'Ha Noi';
+document.write('i = :'+i);
+document.write("<br/>");
+document.write('f = :'+f);
+document.write("<br/>");
+document.write('b = :'+b);
+document.write("<br/>");
+document.write('s = :'+s);
+document.write("<br/>");
+
+// Bai 2
+let width = 20;
+let height = 10;
+let area = width * height;
+document.write("area=: " + area);
+
+
