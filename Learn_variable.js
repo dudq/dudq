143975c55236cfc6d8958090f0598cbf8f6c538b
dudq <dudq.xmt@gmail.com>
2019-09-25 09:09:05 +0700

    Add [exercise] variable,data types and operator

--- Learn_variable.js
+++ Learn_variable.js
@@ -1,4 +1,4 @@
-// Bai 1
+// Bai 1 Khai bao va su dung bien
 /*
 let  i = 10;
 let  f = 20.5;
@@ -22,8 +22,8 @@ let area = width * height;
 document.write("area=: " + area);
 */
 
-//Bai 3
-let inputWidth;
+//Bai 3: Thuc hanh su dung toan tu
+/*let inputWidth;
 let inputHeight;
 inputWidth = prompt("Enter the Width");
 inputHeight = prompt("Enter the Height");
@@ -31,3 +31,34 @@ let width = parseInt(inputWidth);
 let height = parseInt(inputHeight);
 let area = width * height;
 document.write("The area is: " + area);
+*/
+// Bai 4 [Bai tap] Bien, kieu du lieu va toan tu
+//Nhap diem vat ly, hoa hoc, sinh hoc. Tinh diem trung binh, tong
+/*
+let inputPhycis = prompt(" Enter the Phycis score: ");
+let inputChemistry = prompt("Enter the Chemistry score: ");
+let inputBiology = prompt("Enter the Biology score: ");
+let scorePhycis = parseInt(inputPhycis);
+let scoreChemistry = parseInt(inputChemistry);
+let scoreBiology = parseInt(inputBiology);
+let sum = scorePhycis + scoreChemistry + scoreBiology;
+document.write(" Sum: "+ sum + "<br/>");
+let average = sum/3;
+document.write("Average: " + average);
+*/
+
+//Chuyen tu do C sang do F
+/*
+let inputTemperatureC = prompt("Enter the Temperature (C): ");
+let temperatureC = parseInt(inputTemperatureC);
+let temperatureF = (temperatureC*9)/5+32;
+document.write(" The temperature F is: "+ temperatureF);
+*/
+//Tinh dien tich va chu vi hinh tron
+let inputRadius = prompt("Enter the radius:");
+let radius = parseInt(inputRadius);
+const Pi = 3.14;
+let perimeter = 2*radius*Pi;
+let area = Pi*radius^2;
+document.write(" Perimeter is: "+ perimeter + "<br/>");
+document.write(" Area is: " + area);
