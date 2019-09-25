5d3cd6f742dc1246fb38a38f07e352200749cb65
dudq <dudq.xmt@gmail.com>
2019-09-25 13:39:44 +0700

    change nem caculator_demo.html

--- caculator.js
+++ caculator.js
@@ -3,9 +3,27 @@ function get_num1() {
     return _num;
 }
 function get_num2() {
-    let _num = parseInt(document.getElementById("idNum1").value);
+    let _num = parseInt(document.getElementById("idNum2").value);
     return _num;
 }
+function get_id() {
+
+}
+function displayResult(result) {
+    document.getElementById("idResult").innerHTML ="Result:" + result;
+}
+/*
+function caculator() {
+    let num1 = get_num1();
+    let num2 = get_num2();
+    let result;
+    let _addition = document.getElementById("idAddition");
+    let _sutraction = document.getElementById("idSubtraction");
+    let _mutiplication = document.getElementById("idMutiplication");
+    let _division = document.getElementById("idDivision");
+    }
+*/
+
 
 function addition() {
     //let _num1 = parseInt(document.getElementById("idNum1").value);
